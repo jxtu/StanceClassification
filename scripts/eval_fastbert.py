01fe391f2ff09cb2304893120adc6cd9e3d4a9d9
jxtu <jxtu@brandeis.edu>
2020-12-16 21:45:42 -0500

    Added initial results.

--- scripts/eval_fastbert.py
+++ scripts/eval_fastbert.py
@@ -16,8 +16,10 @@ predictor = BertClassificationPredictor(
 
 if __name__ == '__main__':
     test_csv_path = "../data/fast_bert_data/test.csv"
-    texts = pd.read_csv(test_csv_path).text.values.tolist()
+    test_df = pd.read_csv(test_csv_path)
+    texts = test_df.text.values.tolist()
 
     multiple_predictions = predictor.predict_batch(texts)
-    for i in multiple_predictions:
-        print(i)
+    preds = [pred[0][0] for pred in multiple_predictions]
+    test_df["preds"] = preds
+    test_df.to_csv("../data/test_with_pred.csv", index=False)
---
+++ scripts/scoring.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.metrics import precision_score, recall_score, f1_score
+
+
+if __name__ == '__main__':
+    pred_file = "../data/test_with_pred.csv"
+    pred_df = pd.read_csv(pred_file)
+    golds = pred_df.label.values
+    preds = pred_df.preds.values
+    print(f1_score(golds, preds, average="micro"))
