989482888c934e5940db65d1dfa9ec5b6b335128
jxtu <jxtu@brandeis.edu>
2020-12-17 11:51:02 -0500

    Added results from cleaned data.

--- scripts/scoring.py
+++ scripts/scoring.py
@@ -3,7 +3,7 @@ from sklearn.metrics import precision_score, recall_score, f1_score
 
 
 if __name__ == '__main__':
-    pred_file = "../data/test_with_pred.csv"
+    pred_file = "../data/test_with_pred_clean.csv"
     pred_df = pd.read_csv(pred_file)
     golds = pred_df.label.values
     preds = pred_df.preds.values
